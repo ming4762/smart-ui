74fce09e204549dfbbfc25b7c12cb35713a1e3da
shizhongming <zhongming4762@hotmail.com>
2020-09-07 23:01:05 +0800

    添加navigation组件

--- smart-ui-component/src/main/resources/static/js/component/navigation/Navigation.js
+++ smart-ui-component/src/main/resources/static/js/component/navigation/Navigation.js
@@ -9,6 +9,26 @@ define(["require", "exports"], function (require, exports) {
             },
             value: String
         },
-        template: "\n\t<div class=\"content-tabs\">\n\t\t\t<button class=\"roll-nav roll-left\">\n          <a-icon type=\"backward\"/>\n\t\t\t</button>\n\t</div>\n\t"
+        data: function () {
+            return {
+                hoverData: {}
+            };
+        },
+        methods: {
+            handleClick: function (_a) {
+                var key = _a.key;
+                this.$emit('input', key);
+            },
+            handleMouseover: function (_a) {
+                var key = _a.key;
+                console.log('======');
+            },
+            handleRemoveMenu: function (_a, event) {
+                var key = _a.key;
+                this.$emit('remove', key);
+                event.stopPropagation();
+            }
+        },
+        template: "\n\t<div class=\"content-tabs\">\n\t\t\t<div class=\"roll-nav roll-left direction-button\">\n          <a-icon type=\"backward\"/>\n\t\t\t</div>\n\t\t\t<nav class=\"page-tabs J_menuTabs\">\n\t\t\t\t\t<div class=\"page-tabs-content\" style=\"margin-left: 0\">\n\t\t\t\t\t\t\t<a \n\t\t\t\t\t\t\t\thref=\"javascript:;\"\n\t\t\t\t\t\t\t\t@click=\"handleClick(menu)\"\n\t\t\t\t\t\t\t\t:class=\"['J_menuTab', menu.key === value ? 'active' : '']\"\n\t\t\t\t\t\t\t\tv-for=\"(menu, i) in menuList\">\n\t\t\t\t\t\t\t\t\t{{menu.title}}\n                  <a-icon @click=\"handleRemoveMenu(menu, $event)\" :style=\"{color: '#CCCCCC'}\" theme=\"filled\" v-if=\"i !== 0\" type=\"close-circle\" />\n\t\t\t\t\t\t\t</a>\n\t\t\t\t\t</div>\n\t\t\t</nav>\n      <div class=\"roll-nav roll-right direction-button J_tabRight\">\n          <a-icon type=\"forward\" />\n      </div>\n\t\t\t<a-dropdown >\n\t\t\t\t\t<div class=\"btn-group roll-nav roll-right\">\n\t\t\t\t\t\t\t<span>\u5173\u95ED\u64CD\u4F5C</span>\n              <a-icon type=\"caret-down\" />\n\t\t\t\t\t</div>\n\t\t\t\t\t<slot name=\"dropdown-menu\" slot=\"overlay\"></slot>\n\t\t\t</a-dropdown>\n\t</div>\n\t"
     };
 });
--- smart-ui-component/src/main/resources/static/js/component/navigation/Navigation.ts
+++ smart-ui-component/src/main/resources/static/js/component/navigation/Navigation.ts
@@ -11,12 +11,61 @@ export default {
 		},
 		value: String
 	},
+	data () {
+		return {
+			hoverData: {}
+		}
+	},
+	methods: {
+		handleClick ({ key }: any): void {
+			// @ts-ignore
+			this.$emit('input', key)
+		},
+		/**
+		 * 删除图标悬停事件
+		 */
+		handleMouseover ({ key }: any) {
+			console.log('======')
+		},
+		/**
+		 * 移除菜单
+		 * @param key
+		 * @param event 原生事件
+		 */
+		handleRemoveMenu ({ key }: any, event: any): any {
+			// @ts-ignore
+			this.$emit('remove', key)
+			event.stopPropagation()
+		}
+	},
 	// language=html
 	template: `
 	<div class="content-tabs">
-			<button class="roll-nav roll-left">
+			<div class="roll-nav roll-left direction-button">
           <a-icon type="backward"/>
-			</button>
+			</div>
+			<nav class="page-tabs J_menuTabs">
+					<div class="page-tabs-content" style="margin-left: 0">
+							<a 
+								href="javascript:;"
+								@click="handleClick(menu)"
+								:class="['J_menuTab', menu.key === value ? 'active' : '']"
+								v-for="(menu, i) in menuList">
+									{{menu.title}}
+                  <a-icon @click="handleRemoveMenu(menu, $event)" :style="{color: '#CCCCCC'}" theme="filled" v-if="i !== 0" type="close-circle" />
+							</a>
+					</div>
+			</nav>
+      <div class="roll-nav roll-right direction-button J_tabRight">
+          <a-icon type="forward" />
+      </div>
+			<a-dropdown >
+					<div class="btn-group roll-nav roll-right">
+							<span>关闭操作</span>
+              <a-icon type="caret-down" />
+					</div>
+					<slot name="dropdown-menu" slot="overlay"></slot>
+			</a-dropdown>
 	</div>
 	`
 }
